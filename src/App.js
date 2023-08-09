d24ec1e816098fdddb88dd8110b4190f74db1d6a
Emily <emily.zy.lin@gmail.com>
2023-08-09 14:27:51 -0500

    finished all steps, incl the css part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import {useState} from "react";
+import satData from "./components/satData.js"
+
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+
+  //The Set method prevents duplicate elements. Get three orbitTypes: low, medium, and high.
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  
+  //the function filterByType filters satData and saves the results in the variable sat
+  const filterByType = (currentType) =>{
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  }
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    <>
+      <Banner />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />      
+    </>
   );
 }
 
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,25 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+import satData from "./satData.js"
+import "./styling.css"
+
+
+const Buttons = (prop) => {
+
+   return(
+      <div className="flex-container">
+      {prop.displaySats.map((sat, id) => {
+  
+      return (  
+     
+         <button onClick={() => prop.filterByType(sat)} key={id}> 
+             {sat} Orbit
+         </button>
+      )
+      })}
+
+      <button onClick={() => prop.setSat(satData)}>All Orbits</button>
       </div>
-  );
-};
+   );
+
+}
 
 export default Buttons;
